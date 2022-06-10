dcfab674c4efa1398e1e54af30caac9223088fba
Antonin LYAËT <31038778+syukoGit@users.noreply.github.com>
2022-06-10 15:27:16 +0000

    Remove unused useTheme hook

--- src/utils/hooks/useTheme.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-
-function useTheme() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-  return { theme, toggleTheme };
-}
-
-export default useTheme;
