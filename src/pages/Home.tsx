fb25e09ed3ede12f5957b4a0448deb98f14d337f
Antonin LYAËT <31038778+syukoGit@users.noreply.github.com>
2022-06-23 12:15:27 +0000

    Add coming soon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
 import { Box, styled } from "@mui/material";
 
-const StyledBox = styled(Box)(() => ({
+const StyledBox = styled(Box)(({ theme }) => ({
   height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  background: theme.palette.secondary.dark,
+}));
+
+const StyledTitle = styled('h1')(({ theme }) => ({
+  color: theme.palette.primary.light,
+  fontSize: '60px',
+}));
+
+const StyledSubTitle = styled('h3')(({ theme }) => ({
+  color: theme.palette.primary.dark,
+  fontSize: '30px',
 }));
 
 function Home() {
   return (
     <StyledBox>
-      <h1 id="title">Page d'acceuil</h1>
+      <StyledTitle>Antonin LYAËT</StyledTitle>
+      <StyledSubTitle>Coming Soon...</StyledSubTitle>
     </StyledBox>
   );
 }
