c0807004519cd311d9ecbfa8f9bbef73171a07ae
Antonin LYAËT <31038778+syukoGit@users.noreply.github.com>
2022-06-23 13:58:35 +0000

    Apply code styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,29 @@
-import { Box, styled, IconButton, Tooltip } from "@mui/material";
+import { Box, styled, IconButton, Tooltip } from '@mui/material';
 import ThemeSwitch from './ThemeSwitch';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const StyledBox = styled(Box)(({ theme }) => ({
-    display: "flex",
-    justifyContent: "right",
-    alignItems: "center",
-    padding: "0.5rem",
-    backgroundColor: theme.palette.primary.main
+  display: 'flex',
+  justifyContent: 'right',
+  alignItems: 'center',
+  padding: '0.5rem',
+  backgroundColor: theme.palette.primary.main,
 }));
 
 function Header() {
-    return (
-        <StyledBox>
-            <ThemeSwitch />
-            <Tooltip title="GitHub Repository" arrow>
-                <IconButton
-                    children={<GitHubIcon fontSize='inherit'/>}
-                    href="https://github.com/syukoGit/my-web-site"
-                    color='secondary'
-                    size='large' />
-            </Tooltip>
-        </StyledBox>
-    );
+  return (
+    <StyledBox>
+      <ThemeSwitch />
+      <Tooltip title="GitHub Repository" arrow>
+        <IconButton
+          children={<GitHubIcon fontSize="inherit" />}
+          href="https://github.com/syukoGit/my-web-site"
+          color="secondary"
+          size="large"
+        />
+      </Tooltip>
+    </StyledBox>
+  );
 }
 
 export default Header;
--- src/utils/contexts/CustomThemeContext.tsx
+++ src/utils/contexts/CustomThemeContext.tsx
@@ -1,34 +1,31 @@
-import { ThemeProvider } from "@mui/material";
-import { createContext, useMemo, useState } from "react";
-import dark from "../themes/dark";
-import light from "../themes/light";
+import { ThemeProvider } from '@mui/material';
+import { createContext, useMemo, useState } from 'react';
+import dark from '../themes/dark';
+import light from '../themes/light';
 
-export const CustomThemeContext = createContext({ toggleColorMode: () => {}});
+export const CustomThemeContext = createContext({ toggleColorMode: () => {} });
 
 interface ThemeContextProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const CustomThemeProvider = ({ children }: ThemeContextProps) => {
-    const [mode, setMode] = useState<'light' | 'dark'>('dark');
+  const [mode, setMode] = useState<'light' | 'dark'>('dark');
 
-    const colorMode = useMemo(
-        () => ({
-            toggleColorMode: () => {
-                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-            },
-        }), [],
-    );
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
 
-    const theme = useMemo(
-        () => mode === 'light' ? light : dark, [mode]
-    );
+  const theme = useMemo(() => (mode === 'light' ? light : dark), [mode]);
 
-    return (
-        <CustomThemeContext.Provider value={ colorMode }>
-            <ThemeProvider theme={ theme }>
-                {children}
-            </ThemeProvider>
-        </CustomThemeContext.Provider>
-    )
+  return (
+    <CustomThemeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </CustomThemeContext.Provider>
+  );
 };
