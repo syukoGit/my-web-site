09dbea08dc2bd1c738a91907420ba4e02dc6bbf5
syukoGit <syukodev@outlook.com>
2022-06-27 09:05:14 +0200

    Random animation duration + bug fix

--- src/components/AnimatedTriangle.tsx
+++ src/components/AnimatedTriangle.tsx
@@ -1,13 +1,13 @@
 import { styled } from '@mui/material';
 
 const StyledContainer = styled('svg')(
-  ({ $top, $left }: { $top: number; $left: number }) => ({
+  ({ top, left }: { top: number; left: number }) => ({
     position: 'absolute',
-    top: $top,
-    left: $left,
+    top: top,
+    left: left,
     zIndex: 0,
     animationName: 'triangleAnimation',
-    animationDuration: '5s',
+    animationDuration: 5 + Math.random() * (10 - 5) + 's',
     animationIterationCount: 'infinite',
     '@keyframes triangleAnimation': {
       '0%': {
@@ -47,7 +47,7 @@ function AnimatedTriangle({ x, y }: AnimatedTriangleProps) {
   ];
 
   return (
-    <StyledContainer width="150" height="150" $top={x} $left={y}>
+    <StyledContainer width="150" height="150" top={x} left={y}>
       <StyledTriangle points={points.map((p) => `${p.x},${p.y}`).join(' ')} />
       <StyledCircle cx={points[0].x} cy={points[0].y} r="5" />
       <StyledCircle cx={points[1].x} cy={points[1].y} r="5" />
