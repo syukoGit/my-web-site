223c079ef09997fbc78e55b515aa7a73dfeca346
syukoGit <syukodev@outlook.com>
2023-02-10 15:40:18 +0100

    Create a website with a coming soon text

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,9 @@
+import './ComingSoon.scss';
+
+const ComingSoon = () => (
+  <section className="coming-soon">
+    <h1>Coming Soon!</h1>
+  </section>
+);
+
+export default ComingSoon;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from '../components/ComingSoon';
+import TopSection from '../components/Homes/TopSection';
+import './Home.scss';
+
+const Home = () => (
+  <div className="page-home">
+    <TopSection />
+    <ComingSoon />
+  </div>
+);
+
+export default Home;
