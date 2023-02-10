878b8a9aa0d320ba377eb647d8c0e7a5b42cb61b
syukoGit <syukodev@outlook.com>
2023-02-10 17:21:57 +0100

    Remove ComingSoon component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import ComingSoon from '../components/ComingSoon';
 import TopSection from '../components/Homes/TopSection';
 import './Home.scss';
 
 const Home = () => (
   <div className="page-home">
     <TopSection />
-    <ComingSoon />
   </div>
 );
 
